c09cb1de3e7aea46ce5c3515663cde480ea55c62
Radovan Bast <bast@users.noreply.github.com>
2020-06-09 21:07:39 +0200

    about to try upload to PyPI

---
+++ rsh7test.py
@@ -0,0 +1,13 @@
+"""
+Example script to show PyPI upload.
+"""
+
+__version__ = "0.1.0"
+
+
+def something():
+    print("we are calling the something function")
+
+
+def main():
+    print("we are calling the main function")
